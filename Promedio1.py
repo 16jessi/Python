50fd55e8c82ead5455fc878833fc308d17d1a885
16jessi <111916112+16jessi@users.noreply.github.com>
2022-08-25 16:28:19 -0300

    Add files via upload

---
+++ Promedio1.py
@@ -0,0 +1,16 @@
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	num1 = float()
+	num2 = float()
+	num3 = float()
+	total = float()
+	num1 = 10
+	num2 = 6
+	num3 = 6
+	total = (num1+num2+num3)/3
+	print("el promodio de los 3 numeros es de:",total)
+
---
+++ sestructura anidada sino.py
@@ -0,0 +1,8 @@
+n1= int(input("Ingrese el valor para el primer número:"))
+n2= int(input("Ingrese el valor para el segundo número:"))
+if n1>n2:
+   print("n1 es mayor a n2")
+elif n1>=0:
+   print("n1 tiene un valor positivo")
+else:
+   print("n1 tiene un valor negativo")
