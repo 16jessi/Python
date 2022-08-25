f624eea62678611a425eba4ec3d52abd0a87e0e0
16jessi <111916112+16jessi@users.noreply.github.com>
2022-08-25 16:24:35 -0300

    Add files via upload

---
+++ Ejemplo colecc lista 2 py.py
@@ -0,0 +1,8 @@
+li=["1kg papa","2 doc huevos"]
+print("Lista verdulería: ",li)
+li.append("1kg tomate")#añade elelmnto al final de la lista
+print("Lista verdulería: ",li)
+li.insert(0,"1kg cebolla") #inserta elemento en la posición indicada dentro de la lista en este caso en la posición "0"
+print("Lista verdulería: ",li)
+li.pop()#borra el último elemento de la lista
+print("La lista contiene: ",li)
